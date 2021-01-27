1191b1968c58f2cf43a6d97bc311bd14d0d7cd00
Bryan Chafee <bryan.chafee@gmail.com>
2021-01-26 23:48:04 -0500

    -Added testing grounds -Created empty environment heirarchy prefab -Created basic RunMan prefab with temporary parts -Created basic Scale Ability script that controls increasing/decreasing the scale and mass of a game object, uses mousewheel -Created basic Physics based movement script for testing, functionality will need to be removed from this as game develops -Created utility script for passively following objects in a non linear fashion, tried using it on camera to see how it felt but took it off in the end. May have unforeseen uses elsewhere. -Created a simple sky box so i didn't have to look at the default unity sky box, definitely not final -Added a Floor and Wall tag that will be used primarily by the agent script(s?) in the future, currently used for a jump feature in the movement script that we will eventually remove according to current design.

---
+++ Assets/Scripts/Utility/FollowObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    /// <summary>
+    /// Follow Object class follows an assigned target object
+    /// </summary>
+    public class FollowObject : MonoBehaviour
+    {
+        [Tooltip("The game object to be followed by this object")]
+        public GameObject _Target;
+
+        [Tooltip("How quickly the camera moves towards the targets position")]
+        public float _Speed = 1;
+
+        [Tooltip("Offset this object has from the target objects transform")]
+        public Vector3 _Offset;
+
+        /// <summary>
+        /// Non-linearly translates from local object's current position to a target object's position.
+        /// This script could be expanded to have toggles choosing the style of non-linear transition.
+        /// </summary>
+        private void FixedUpdate()
+        {
+            var position = _Target.transform.position;
+            transform.localPosition = Vector3.Lerp(transform.localPosition,
+                new Vector3(_Offset.x, _Offset.y, _Offset.z), Time.deltaTime * _Speed);
+        }
+    }
+}
