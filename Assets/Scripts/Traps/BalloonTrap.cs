35a44d64795f51c4e24b2c2f5cf89160a6959aae
SarahPak <sarahshp99@gmail.com>
2021-03-28 15:57:44 -0400

    Added Sarah Levels and balloon trap prefab

--- Assets/Scripts/Traps/BalloonTrap.cs
+++ Assets/Scripts/Traps/BalloonTrap.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BalloonTrap : MonoBehaviour
 {
+    public float timerate = 0.5f; //lower value will make it inflate and deflate more slowly
+    public int growthfactor = 12; //greater value will make it inflate and deflate more drastically
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,6 @@ public class BalloonTrap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float timerate = 0.5f; //lower value will make it inflate and deflate more slowly
-        int growthfactor = 12; //greater value will make it inflate and deflate more drastically
                 
         //uses sign function to increase and decrease its scale overtime
         Vector3 vec = new Vector3(growthfactor * Mathf.Sin(Time.time * timerate), growthfactor * Mathf.Sin(Time.time*timerate), growthfactor * Mathf.Sin(Time.time*timerate));
